24cbcacb9bbd12236e6aa81564d906e7d9d83b87
Caux <pierre-damien_caux@hotmail.fr>
2021-03-12 17:59:32 +0100

    Resolve pb

--- NintendoProject/src/main/java/test/Test.java
+++ NintendoProject/src/main/java/test/Test.java
@@ -1,5 +1,6 @@
 package test;
 
+import metier.Boutique;
 import metier.Client;
 import metier.Console;
 import metier.Jeu;
@@ -12,14 +13,19 @@ public class Test {
 	public static void main(String[] args) {
 		Console PS5 = new Console ("PS5",500.0,"2020");
 		Console XBOXONE = new Console ("XBOXONE",600.0,"2019");
-		List <Console> consoles = {PS5,XBOXONE};
-		Jeu jeu1 = new Jeu("Mario",consoles, B1);
-		Jeu jeu2 = new Jeu("Zelda",consoles, B2);
-		Boutique b1 = new Boutique("B1","34 rue boileau", jeux);
-		Boutique b2 = new Boutique("B2","35 rue boileau", jeux);
+		ArrayList <Console> consoles = new ArrayList<Console>();
+		consoles.add(XBOXONE);
+		consoles.add(PS5);
+		Jeu jeu1 = new Jeu("Mario",consoles);
+		Jeu jeu2 = new Jeu("Zelda",consoles);
 		ArrayList<Jeu> jeux = new ArrayList<Jeu>();
 		jeux.add(jeu1);
 		jeux.add(jeu2);
+		Boutique b1 = new Boutique("B1","34 rue boileau", jeux);
+		Boutique b2 = new Boutique("B2","35 rue boileau", jeux);
+		jeu1.setBoutique(b1);
+		jeu2.setBoutique(b2);
+		
 		Client client1 = new Client("Jordan", "Abid", jeux);
 	}
 
