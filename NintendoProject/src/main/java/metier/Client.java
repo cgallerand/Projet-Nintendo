9be75b437d1693cb06ca228105fda3731d00c2bc
Amandine <amandine.leroy@soprasteria.com>
2021-03-12 16:57:41 +0100

    Ajout Classe Client

---
+++ NintendoProject/src/main/java/metier/Client.java
@@ -0,0 +1,39 @@
+package metier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+	String nom;
+	String prenom;
+	List<Jeu> jeu= new ArrayList();
+	public Client(String nom, String prenom, List<Jeu> jeu) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.jeu = jeu;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public List<Jeu> getJeu() {
+		return jeu;
+	}
+	public void setJeu(List<Jeu> jeu) {
+		this.jeu = jeu;
+	}
+	@Override
+	public String toString() {
+		return "Client [nom=" + nom + ", prenom=" + prenom + ", jeu=" + jeu + "]";
+	}
+
+	
+}
