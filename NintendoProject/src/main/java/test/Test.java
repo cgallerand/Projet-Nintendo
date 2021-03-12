2ea093f74d92ad19741b9f0e41a6a40dfc4d7f4b
Amandine <amandine.leroy@soprasteria.com>
2021-03-12 17:53:11 +0100

    modification classe boutique et main

--- NintendoProject/src/main/java/test/Test.java
+++ NintendoProject/src/main/java/test/Test.java
@@ -11,8 +11,10 @@ public class Test {
 		Console PS5 = new Console ("PS5",500.0,"2020");
 		Console XBOXONE = new Console ("XBOXONE",600.0,"2019");
 		List <Console> consoles = {PS5,XBOXONE};
-		Jeu jeu1 = new Jeu("Mario",consoles);
-		Jeu jeu2 = new Jeu("Zelda",consoles);
+		Jeu jeu1 = new Jeu("Mario",consoles, B1);
+		Jeu jeu2 = new Jeu("Zelda",consoles, B2);
+		Boutique b1 = new Boutique("B1","34 rue boileau", jeux);
+		Boutique b2 = new Boutique("B2","35 rue boileau", jeux);
 	}
 
 }
